2a8acd4f41487c41559198866bd50025af5cd3ac
pratik mane <pratik.mane1299@gmail.com>
2020-06-14 14:15:41 +0530

    complete user feed :sparkles:

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const userFeedQuery = gql`
+  query posts {
+    posts {
+      id
+      title
+      content
+      thumbnail
+      createdAt
+      author {
+        username
+      }
+    }
+  }
+`;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor() {}
+  feed: [] = [];
+
+  loading = false;
+  querySubscription: Subscription;
+
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit() {
+    this.loadFeed();
+  }
+
+  ngOnDestroy() {
+    if (this.querySubscription) {
+      this.querySubscription.unsubscribe();
+    }
+  }
+
+  loadFeed() {
+    this.loading = true;
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: userFeedQuery
+    })
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        this.feed = data.posts;
+        this.loading = loading;
+      });
+  }
 
 }
